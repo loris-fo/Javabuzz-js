519d36d7db0b8e34105fa6c3c954f61941646c7d
lorisfo <loris-fo@outlook.com>
2015-03-03 12:49:57 +0000

    added same feature for 5 and 15

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -2,11 +2,14 @@ describe('Javabuzz',function() {
 
   var javabuzz;
 
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  })
+
   describe('Knows when a number is', function() {
 
     it('Divisible by 3', function() {
 
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
 
     });
@@ -17,10 +20,50 @@ describe('Javabuzz',function() {
 
     it('Divisible by 3', function() {
 
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(1)).toBe(false);
 
     });
+
+  });
+
+  describe('Knows when a number is', function() {
+
+    it('Divisible by 5', function() {
+
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+
+    });
+
+  });
+
+  describe('Knows when a number is Not', function() {
+
+    it('Divisible by 5', function() {
+
+      expect(javabuzz.isDivisibleByFive(4)).toBe(false);
+
+    });
+
+  });
+
+  describe('Knows when a number is', function() {
+
+    it('Divisible by 15', function() {
+
+      expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
+
+    });
+
+  });
+
+  describe('Knows when a number is Not', function() {
+
+    it('Divisible by 15', function() {
+
+      expect(javabuzz.isDivisibleByFifteen(12)).toBe(false);
+
+    });
+
   });
 
 });
