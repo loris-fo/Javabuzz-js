4413347a9a86b80438a77344b0a8f91d9354ad5d
lorisfo <loris-fo@outlook.com>
2015-03-03 12:29:25 +0000

    Knows when a number is and is NOT divisible by three

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -13,4 +13,14 @@ describe('Javabuzz',function() {
 
   });
 
+  describe('Knows when a number is Not', function() {
+
+    it('Divisible by 3', function() {
+
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+
+    });
+  });
+
 });
